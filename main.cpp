fa0085f8d12d2f6d67acdd4658244df2e4ca370f
yshi80 <yshi80@ford.com>
2024-05-30 10:37:41 +0800

    add vec3.h, color.h

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 #include <fstream>
 
@@ -9,22 +12,14 @@ int main() {
     int image_height = 256;
 
     // Render
-    std::ofstream outfile("../PPMTest.ppm", std::ios_base::out);
+    std::ofstream outfile("PPMTest.ppm", std::ios_base::out);
     outfile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-    // std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     for (int j = 0; j < image_height; j++) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        std::clog << "\rScan lines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            outfile << ir << ' ' << ig << ' ' << ib << '\n';
-            // std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+            write_color(std::cout,outfile, pixel_color);
         }
     }
     std::clog << "\rDone.\n";
