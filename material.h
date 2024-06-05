ebd17ff146ca6449f531cd8219fbc4dd296fd284
yshi80 <yshi80@ford.com>
2024-06-05 16:17:04 +0800

    增加漫反射材质 lambertian

--- material.h
+++ material.h
@@ -6,8 +6,9 @@
 #define MATERIAL_H
 
 #include "rt_constants.h"
+#include "hittable.h"
 
-class hit_record;
+// class hit_record;
 
 class material {
 public:
@@ -16,6 +17,25 @@ public:
     virtual bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const {
         return false;
     }
+};
+
+class lambertian : public material {
+public:
+    lambertian(const color &albedo) : albedo(albedo) {}
+
+    bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered)
+    const override {
+        auto scatter_direction = rec.normal + random_unit_vector();
+        // Catch degenerate scatter direction
+        if (scatter_direction.near_zero())
+            scatter_direction = rec.normal;
+        scattered = ray(rec.p, scatter_direction);
+        attenuation = albedo;
+        return true;
+    }
+
+private:
+    color albedo;
 };
 
 #endif // MATERIAL_H
--- rt_constants.h
+++ rt_constants.h
@@ -17,6 +17,7 @@ using std::make_shared;
 using std::shared_ptr;
 using std::sqrt;
 using std::string;
+using std::fabs;
 
 //Constants
 const double infinity = std::numeric_limits<double>::infinity();
