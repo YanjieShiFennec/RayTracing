7768bea7dc9a5961fbd31433bdcecef8927f2d62
yshi80 <yshi80@ford.com>
2024-06-06 14:43:29 +0800

    增加折射材质电介质 dielectric

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main() {
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.0);
+    auto material_left = make_shared<dielectric>(1.5);
     auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100, material_ground));
@@ -27,7 +27,7 @@ int main() {
     camera cam;
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 1080;
+    cam.image_width = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
 
