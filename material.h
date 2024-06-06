8135a3ea4e6a19ce5395e0fd8fa02131544e25f2
yshi80 <yshi80@ford.com>
2024-06-06 15:16:57 +0800

    折射材质模拟菲涅尔现象

--- material.h
+++ material.h
@@ -76,8 +76,8 @@ public:
 
         bool cannot_refract = ri * sin_theta > 1.0; // 折射还是全反射
         vec3 direction;
-        if(cannot_refract)
-            direction = reflect(unit_direction,rec.normal);
+        if (cannot_refract || reflectance(cos_theta, ri) > random_double())
+            direction = reflect(unit_direction, rec.normal);
         else
             direction = refract(unit_direction, rec.normal, ri);
 
@@ -89,6 +89,14 @@ private:
     // Refractive index in vacuum or air, or the ratio of the material's refractive index over
     // the refractive index of the enclosing media.
     double refraction_index;
+
+    static double reflectance(double cosine, double refraction_index) {
+        // Use Schlick's approximation for reflectance
+        // 模拟菲涅尔现象
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * pow((1 - cosine), 5);
+    }
 };
 
 #endif // MATERIAL_H
