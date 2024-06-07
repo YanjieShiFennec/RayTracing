2703451c0a260e0a67bba4b7cdee4ca91d691913
yshi80 <yshi80@ford.com>
2024-06-07 09:51:16 +0800

    实现镜头移动

--- main.cpp
+++ main.cpp
@@ -14,25 +14,25 @@ int main() {
     // 设置球体
     hittable_list world;
 
-    auto R = cos(pi/4);
-
-    auto material_left = make_shared<lambertian>(color(0,0,1));
-    auto material_right = make_shared<lambertian>(color(1,0,0));
-
-    world.add(make_shared<sphere>(point3(-R,0,-1),R,material_left));
-    world.add(make_shared<sphere>(point3(R,0,-1),R,material_right));
-
-    // auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    // auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    // auto material_left = make_shared<dielectric>(1.5);
-    // auto material_bubble = make_shared<dielectric>(1.0/1.5); // 在 material_left 中嵌入 空气泡，material_left 即为空心玻璃球
-    // auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+    // auto R = cos(pi/4);
     //
-    // world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100, material_ground));
-    // world.add(make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
-    // world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-    // world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_bubble));
-    // world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+    // auto material_left = make_shared<lambertian>(color(0,0,1));
+    // auto material_right = make_shared<lambertian>(color(1,0,0));
+    //
+    // world.add(make_shared<sphere>(point3(-R,0,-1),R,material_left));
+    // world.add(make_shared<sphere>(point3(R,0,-1),R,material_right));
+
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left = make_shared<dielectric>(1.5);
+    auto material_bubble = make_shared<dielectric>(1.0 / 1.5); // 在 material_left 中嵌入 空气泡，material_left 即为空心玻璃球
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+
+    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100, material_ground));
+    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_bubble));
+    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     camera cam;
 
@@ -41,7 +41,10 @@ int main() {
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
 
-    cam.vfov = 60;
+    cam.vfov = 30;
+    cam.lookfrom = point3(-4, 1, 1);
+    cam.lookat = point3(0, 0, -1);
+    cam.vup = vec3(0, 1, 0);
 
     // 渲染计时
     clock_t start, end;
