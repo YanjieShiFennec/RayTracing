e8029e67cfa91596622b8cc5cd94acbf6d6e3740
yshi80 <yshi80@ford.com>
2024-06-20 14:28:04 +0800

    ray with time information

--- ray.h
+++ ray.h
@@ -11,11 +11,15 @@ class ray {
 public:
     ray() {}
 
-    ray(const point3 &origin, const vec3 &direction) : orig(origin), dir(direction) {}
+    ray(const point3 &origin, const vec3 &direction) : orig(origin), dir(direction), tm(0.0f) {}
+
+    ray(const point3 &origin, const vec3 &direction, float time) : orig(origin), dir(direction), tm(time) {}
 
     const point3 &origin() const { return orig; }
 
     const vec3 &direction() const { return dir; }
+
+    float time() const { return tm; }
 
     point3 at(float t) const {
         return orig + t * dir;
@@ -26,6 +30,7 @@ private:
     // 根据输入 t 确定最终光线到达的点 P: point3 at(float t)
     point3 orig; // 光源坐标
     vec3 dir; // 光线方向
+    float tm;
 };
 
 #endif // RAY_H
