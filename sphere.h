c703e866a83fdd96000dc933904bacbe2bdfd751
yshi80 <yshi80@ford.com>
2024-06-20 14:33:39 +0800

    add moving spheres

--- sphere.h
+++ sphere.h
@@ -11,8 +11,15 @@
 
 class sphere : public hittable {
 public:
+    // Stationary Sphere
     sphere(const point3 &center, float radius, shared_ptr<material> mat)
-    : center(center), radius(fmax(0, radius)),mat(mat) {}
+            : center1(center), radius(fmax(0.0f, radius)), mat(mat), is_moving(false) {}
+
+    // Moving sphere
+    sphere(const point3 &center1, const point3 &center2, float radius, shared_ptr<material> mat) :
+            center1(center1), radius(fmax(0.0f, radius)), mat(mat), is_moving(true) {
+        center_vec = center2 - center1;
+    }
 
     bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
         /*
@@ -26,6 +33,7 @@ public:
         * 其中 t 为未知数，展开得 (d·d)t^2 + (-2d·(C-Q))t + (C-Q)·(C-Q) - r^2 = 0
         * 二元一次方程 b^2 - 4ac >= 0 时有解，说明光线击中球体
         */
+        point3 center = is_moving ? sphere_center(r.time()) : center1;
         vec3 oc = center - r.origin();
         // auto a = dot(r.direction(), r.direction());
         // auto b = -2.0 * dot(r.direction(), oc);
@@ -61,9 +69,17 @@ public:
     }
 
 private:
-    point3 center;
+    point3 center1;
     float radius;
     shared_ptr<material> mat;
+    bool is_moving;
+    vec3 center_vec;
+
+    point3 sphere_center(float time) const {
+        // Linearly interpolate from center1 to center2 according to time, where t = 0 yields
+        // center1, and t = 1 yields center2.
+        return center1 + time * center_vec;
+    }
 };
 
 #endif // SPHERE_H
