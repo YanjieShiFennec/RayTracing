6223541108613be24bb1f7ec7d2e752e585d1494
yshi80 <yshi80@ford.com>
2024-06-25 17:13:59 +0800

    init binary radix tree

---
+++ binary_radix_tree.cpp
@@ -0,0 +1,138 @@
+#include <vector>
+#include <cmath>
+#include <cstdint>
+#include <algorithm>
+#include <limits>
+#include <iostream>
+
+// Tree node types
+enum class PtrType {
+    Leaf,
+    Inner
+};
+
+class node {
+public:
+    int value;
+    PtrType type;
+
+    node(int value, PtrType type) : value(value), type(type) {}
+};
+
+class inner_node : public node {
+public:
+    node *left;
+    node *right;
+
+    inner_node() : node(0, PtrType::Inner) {}
+
+    inner_node(node *left, node *right, int value) : node(value, PtrType::Inner), left(left), right(right) {}
+};
+
+class leaf_node : public node {
+public:
+    leaf_node() : node(0, PtrType::Leaf) {}
+
+    leaf_node(int value) : node(value, PtrType::Leaf) {}
+};
+
+// 1 代表开始，-1 代表结尾
+int delta(const int *data, int size, int i, int j) {
+    if (j >= 0 && j < size) {
+        unsigned int di = *(data + i);
+        unsigned int dj = *(data + j);
+
+        // Handle duplicates by using index as tiebreaker if necessary.
+        if (di == dj) {
+            return 32 + __builtin_clz(static_cast<uint32_t>(i) ^ static_cast<uint32_t>(j));
+        }
+        return __builtin_clz(di ^ dj);
+    }
+    return -1;
+}
+
+// Helper function for dividing with rounding up
+int div_rounding_up(int x, int y) {
+    return (x + y - 1) / y;
+}
+
+inner_node *generateHierarchy(int *data, int size) {
+    leaf_node *leaf_nodes = new leaf_node[size];
+    inner_node *inner_nodes = new inner_node[size - 1];
+    // leaf 结点存放数值
+    for (int i = 0; i < size; i++)
+        leaf_nodes[i].value = data[i];
+
+    for (int i = 0; i < size - 1; i++) {
+        // Determine direction of range
+        int d = delta(data, size, i, i + 1) < delta(data, size, i, i - 1) ? -1 : 1;
+
+        // Compute upper bound for the length of the range
+        int delta_min = delta(data, size, i, i - d);
+        int l_max = 2;
+        while (delta(data, size, i, i + l_max * d) > delta_min)
+            l_max *= 2;
+
+        // Find the other end using binary search
+        int l = 0;
+        // 二分查找
+        for (int t = l_max / 2; t >= 1; t /= 2) {
+            if (delta(data, size, i, i + (l + t) * d) > delta_min)
+                l += t;
+            else
+                break;
+        }
+        int j = i + l * d;
+
+        // Find the split position using binary search
+        int delta_node = delta(data, size, i, j);
+        int s = 0, q = 1, t;
+        // 二分查找
+        while (q <= l) {
+            t = div_rounding_up(l, q * 2);
+            if (delta(data, size, i, i + (s + t) * d) > delta_node) {
+                s += t;
+            }
+            q *= 2;
+        }
+        int gamma = i + s * d + std::min(d, 0);
+
+        // Output child pointers
+        if (std::min(i, j) == gamma)
+            inner_nodes[i].left = &leaf_nodes[gamma];
+        else
+            inner_nodes[i].left = &inner_nodes[gamma];
+        if (std::max(i, j) == gamma + 1)
+            inner_nodes[i].right = &leaf_nodes[gamma + 1];
+        else
+            inner_nodes[i].right = &inner_nodes[gamma + 1];
+        // inner 结点存放 index
+        inner_nodes[i].value = i;
+    }
+
+    return &inner_nodes[0];
+};
+
+void print(inner_node *n) {
+    std::cout << (n->type == PtrType::Leaf ? "Leaf" : "Inner") << n->value << std::endl;
+    if (n->type == PtrType::Leaf) {
+        return;
+    }
+    print((inner_node *) n->left);
+    print((inner_node *) n->right);
+}
+
+int main() {
+    int size = 8;
+    int *data = new int[size]{1, 2, 4, 5, 19, 24, 25, 30};
+    // for (int i = 0; i < size; i++) {
+    //     *(data + i) = i + 1;
+    // }
+    //
+    inner_node *in = generateHierarchy(data, size);
+    print(in);
+
+    delete[]data;
+    delete in;
+    return 0;
+}
