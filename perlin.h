b703a28f6442475261721aab58a9f85b6135ab04
yshi80 <yshi80@ford.com>
2024-08-16 14:13:41 +0800

    Perlin texture, shifted off integer values

--- perlin.h
+++ perlin.h
@@ -7,7 +7,7 @@ class perlin {
 public:
     perlin() {
         for (int i = 0; i < point_count; i++)
-            randfloat[i] = random_float();
+            randvec[i] = unit_vector(vec3::random(-1, 1));
 
         perlin_generate_perm(perm_x);
         perlin_generate_perm(perm_y);
@@ -19,30 +19,26 @@ public:
         auto v = p.y() - std::floorf(p.y());
         auto w = p.z() - std::floorf(p.z());
 
-        // use a Hermite cubic to round off the interpolation
-        u = u * u * (3 - 2 * u);
-        v = v * v * (3 - 2 * v);
-        w = w * w * (3 - 2 * w);
-
         auto i = int(std::floorf(p.x()));
         auto j = int(std::floorf(p.y()));
         auto k = int(std::floorf(p.z()));
 
         // 进行三线性插值平滑噪声
-        float c[2][2][2];
+        vec3 c[2][2][2];
         for (int di = 0; di < 2; di++)
             for (int dj = 0; dj < 2; dj++)
                 for (int dk = 0; dk < 2; dk++)
                     // x & 255: 将 x 截断成 0 - 255 的整数
                     // ^ 进行异或哈希
-                    c[di][dj][dk] = randfloat[perm_x[(i + di) & 255] ^ perm_x[(j + dj) & 255] ^ perm_x[(k + dk) & 255]];
+                    c[di][dj][dk] = randvec[perm_x[(i + di) & 255] ^ perm_x[(j + dj) & 255] ^ perm_x[(k + dk) & 255]];
 
-        return trilinear_interp(c, u, v, w);
+        return perlin_interp(c, u, v, w);
     }
 
 private:
     static const int point_count = 256;
     float randfloat[point_count];
+    vec3 randvec[point_count];
     int perm_x[point_count];
     int perm_y[point_count];
     int perm_z[point_count];
@@ -62,17 +58,26 @@ private:
         }
     }
 
-    static float trilinear_interp(float c[2][2][2], float u, float v, float w) {
+    static float perlin_interp(const vec3 c[2][2][2], float u, float v, float w) {
+        // use a Hermite cubic to round off the interpolation
+        auto uu = u * u * (3 - 2 * u);
+        auto vv = v * v * (3 - 2 * v);
+        auto ww = w * w * (3 - 2 * w);
+
         // 三线性插值
         auto accum = 0.0f;
         for (int i = 0; i < 2; i++)
             for (int j = 0; j < 2; j++)
-                for (int k = 0; k < 2; k++)
-                    accum += (i * u + (1 - i) * (1 - u))
-                             * (j * v + (1 - j) * (1 - v))
-                             * (k * w + (1 - k) * (1 - w))
-                             * c[i][j][k];
-        return accum;
+                for (int k = 0; k < 2; k++) {
+                    vec3 weight_v(u - i, v - j, w - k);
+                    accum += (i * uu + (1 - i) * (1 - uu))
+                             * (j * vv + (1 - j) * (1 - vv))
+                             * (k * ww + (1 - k) * (1 - ww))
+                             * dot(c[i][j][k], weight_v);
+                }
+
+        // map accum range from [-1, +1] to [0, 1]
+        return 0.5f * (1.0f + accum);
     }
 };
 
